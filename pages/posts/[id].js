2df74da3a78066298aafaa731282ecc964bf9a2c
Ayushi Jhinjhore <Ayushijhinjhore12@gmail.com>
2024-07-30 15:06:18 +0530

    done

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,12 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 function PostDetail() {
+  const router = useRouter();
+  console.log(router.id)
   // Complete the function
+  return(
+    <h1 id={router.query.id}>Post Details: {router.query.id}</h1>
+  )
 }
 
 export default PostDetail;
